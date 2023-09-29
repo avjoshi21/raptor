ef6c63d615b5e3a9d1dafb4b07898f2bee5dc4be
Abhishek Joshi <avjoshi2@illinois.edu>
2023-09-29 15:02:37 -0500

    better plotting code for comparison

--- compare_codes.py
+++ compare_codes.py
@@ -22,21 +22,45 @@ def loadRaptorIm(raptorim):
 def compare_images(ipoleim,raptorim):
     ipoleData = loadIpoleIm(ipoleim)
     raptorData = loadRaptorIm(raptorim)
-
+    stokesVector = ['I','Q','U','V']
     # fig,axs=plt.subplots(1,2);
-    fig=plt.figure()
+    fig=plt.figure(figsize=(6,12))
     axs = ImageGrid(fig,111,
-            nrows_ncols = (2,1),
-            axes_pad = 0.1,
+            nrows_ncols = (2,4),
+            axes_pad = (0.5,0.1),
             cbar_location = "right",
             cbar_mode="each",
             cbar_size="5%",
             cbar_pad=0.05
             )
-    im0=axs[0].imshow(ipoleData['I'],origin='lower',cmap='afmhot')
-    im1=axs[1].imshow(raptorData[:,:,2].T,origin='lower',cmap='afmhot')
-    plt.colorbar(im0,cax=axs.cbar_axes[0])
-    plt.colorbar(im1,cax=axs.cbar_axes[1])
+    for i in range(4):
+      ipoleData[stokesVector[i]]*=ipoleData['scale']
+      dataLims = [0.007,0.0015,0.0015,0.00025]
+      vmax0=dataLims[i]
+      vmax1=dataLims[i]
+      if i==0:
+        cmap = 'afmhot'
+        vmin0=0
+        vmin1=0
+        #vmax0=np.max(ipoleData[stokesVector[i]])
+        #vmax1=np.max(raptorData[:,:,2+i])
+      else:
+        cmap = 'seismic'
+        #vmax0=np.max(ipoleData[stokesVector[i]])
+        #vmax1=np.max(raptorData[:,:,2+i])
+        vmin0=-vmax0
+        vmin1=-vmax1
+      axs[i].set_title(stokesVector[i])
+      im0=axs[i].imshow(ipoleData[stokesVector[i]],origin='lower',cmap=cmap,vmin=vmin0,vmax=vmax0)
+      im1=axs[4+i].imshow(raptorData[:,:,2+i].T,origin='lower',cmap=cmap,vmin=vmin1,vmax=vmax1)
+      cb0 = plt.colorbar(im0,cax=axs.cbar_axes[i])
+      cb0.ax.tick_params(labelsize=5)
+      cb1 = plt.colorbar(im1,cax=axs.cbar_axes[4+i])
+      cb1.ax.tick_params(labelsize=5)
+      axs[i].tick_params(axis='both',bottom=False,left=False,labelbottom=False,labelleft=False)
+      axs[i+4].tick_params(axis='both',left=False,bottom=False,labelbottom=False,labelleft=False)
+    axs[0].set_ylabel("ipole")
+    axs[4].set_ylabel("raptor")
     plt.subplots_adjust(left=0,right=1,bottom=0,top=1,wspace=0,hspace=0)
     plt.savefig("ipole_raptor_comp.png",dpi=200,bbox_inches="tight")
 
